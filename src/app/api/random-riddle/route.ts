b6522a85f622edc5212dbaedad21a8c1afd31c06
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-05-23 18:07:44 +0200

    feat: Enable to exclude riddle from random query

--- src/app/api/random-riddle/route.ts
+++ src/app/api/random-riddle/route.ts
@@ -1,8 +1,11 @@
+import { NextRequest } from 'next/server';
 import db from '../../../../db';
 
-export async function GET() {
-  const { riddles} = db;
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const excluded = searchParams.get('excluded');
+  const riddles = db.riddles.filter((riddle) => riddle.id !== excluded);
   const random = riddles[Math.floor(Math.random() * riddles.length)];
 
-  return Response.json({ id: random.id })
+  return Response.json({ id: random.id });
 }
